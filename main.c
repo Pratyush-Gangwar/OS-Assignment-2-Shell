60b48e182958f5ad698e5763d13272a731fdd204
Pratyush Gangwar <pratyush23395@iiitd.ac.in>
2024-09-20 09:05:45 +0530

    Added code for print_prompt
    Get username via getlogin_r
    Get machine name via gethostname
    Get current working directory via getcwd

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define MAXLEN 1000
 
 void print_prompt() {
+    char cwd[MAXLEN];
+    if (getcwd(cwd, MAXLEN) == NULL) {
+        perror("print_promot getcwd error: ");
+        exit(1);
+    }
+
+    char username[MAXLEN];
+    if( getlogin_r(username, MAXLEN) != 0 ) {
+        perror("print_promt getlogin_r error: ");
+        exit(1);
+    }
+
+    char machinename[MAXLEN];
+    if( gethostname(machinename, MAXLEN) == -1) {
+        perror("print_prompt gethostname error: ");
+        exit(1);
+    }
+
+    printf("%s@%s:%s$ ", username, machinename, cwd);
 }
 
 void read_user_input() {
